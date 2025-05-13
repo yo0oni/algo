1c993fdc38fb808020bf9983384474810c6ecbe9
Jiyoon Kim <77226122+yo0oni@users.noreply.github.com>
2025-05-13 14:49:24 +0900

    [level 3] Title: 숫자 게임, Time: 149.14 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        PriorityQueue<Integer> apq = new PriorityQueue<>((a, b) -> b - a);
+        PriorityQueue<Integer> bpq = new PriorityQueue<>((a, b) -> b - a);
+        
+        for(int i = 0; i < B.length; i ++) {
+            apq.add(A[i]);
+            bpq.add(B[i]);
+        }
+        
+        for(int i = 0; i < B.length; i ++) {
+            if(apq.peek() < bpq.peek()) {
+                answer++;
+                apq.poll();
+                bpq.poll();
+            } else {
+                apq.poll();
+            }
+        }
+        
+        return answer;
+    }
+}
