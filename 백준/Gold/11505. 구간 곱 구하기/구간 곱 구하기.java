8cada89390054ac69ac3fa4d7c3df14751120c8a
Jiyoon Kim <77226122+yo0oni@users.noreply.github.com>
2025-05-15 16:10:34 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 468 ms, Memory: 109880 KB -BaekjoonHub

---
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	static int N, M, K;
+	static long[] arr, tree;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		arr = new long[N];
+		tree = new long[N * 4];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Long.parseLong(br.readLine());
+		}
+
+		init(1, 0, N - 1);
+
+		for (int i = 0; i < M + K; i++) {
+			st = new StringTokenizer(br.readLine());
+			long a = Long.parseLong(st.nextToken());
+			long b = Long.parseLong(st.nextToken());
+			long c = Long.parseLong(st.nextToken());
+
+			if (a == 1) {
+				update(1, 0, N - 1, b - 1, c);
+			} else {
+				sb.append(query(1, 0, N - 1, b - 1, c - 1)).append("\n");
+			}
+		}
+		System.out.println(sb);
+	}
+
+	private static long query(int node, int start, int end, long left, long right) {
+		if (right < start || end < left)
+			return 1;
+		if (left <= start && end <= right)
+			return tree[node];
+
+		int mid = (start + end) / 2;
+		return query(node * 2, start, mid, left, right) * query(node * 2 + 1, mid + 1, end, left, right) % 1_000_000_007;
+	}
+
+	private static long init(int node, int start, int end) {
+		if (start == end)
+			return tree[node] = arr[start];
+
+		int mid = (start + end) / 2;
+		return tree[node] = init(node * 2, start, mid) * init(node * 2 + 1, mid + 1, end) % 1_000_000_007;
+	}
+
+	private static void update(int node, int start, int end, long idx, long val) {
+		if (idx < start || end < idx)
+			return;
+
+		if (start == end) {
+			tree[node] = val;
+			return;
+		}
+
+		int mid = (start + end) / 2;
+		update(node * 2, start, mid, idx, val);
+		update(node * 2 + 1, mid + 1, end, idx, val);
+		tree[node] = tree[node * 2] * tree[node * 2 + 1] % 1_000_000_007;
+
+	}
+}
