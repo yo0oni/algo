6dd843a6fd31fc63083b63385234a5d032e0e56c
Jiyoon Kim <77226122+yo0oni@users.noreply.github.com>
2025-05-20 09:20:46 +0900

    [Bronze III] Title: 최소, 최대, Time: 508 ms, Memory: 90208 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		
+		while(st.hasMoreTokens()) {
+			int num = Integer.parseInt(st.nextToken());
+			if(num > max) {
+				max = num;
+			}
+			if(num < min) {
+				min = num;
+			}
+		}
+		System.out.println(min + " " + max);
+	}
+}
