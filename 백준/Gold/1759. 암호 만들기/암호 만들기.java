6a108dbf77b3db04871a68c80d98440e8ff73414
Jiyoon Kim <77226122+yo0oni@users.noreply.github.com>
2025-06-08 15:51:12 +0900

    [Gold V] Title: 암호 만들기, Time: 116 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int L;
+	static int C;
+	static String[] words;
+	static StringBuilder sb = new StringBuilder();
+	static ArrayDeque<String> result = new ArrayDeque<>();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		L = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		words = new String[C];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < C; i++) {
+			words[i] = st.nextToken();
+		}
+
+		Arrays.sort(words);
+
+		dfs(0, 0, new StringBuilder());
+
+		while (!result.isEmpty()) {
+			sb.append(result.poll()).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+	public static void dfs(int depth, int start, StringBuilder current) {
+		if (current.length() == L) {
+			if (isValid(current.toString()) && !result.contains(current.toString())) {
+				result.add(current.toString());
+			}
+			return;
+		}
+
+		for (int i = start; i < C; i++) {
+			current.append(words[i]);
+			dfs(depth + 1, i + 1, current);
+			current.deleteCharAt(current.length() - 1);
+		}
+	}
+
+	private static boolean isValid(String str) {
+		int vowels = 0, consonants = 0;
+
+		for (char c : str.toCharArray()) {
+			if ("aeiou".indexOf(c) >= 0)
+				vowels++;
+			else
+				consonants++;
+		}
+
+		return vowels >= 1 && consonants >= 2;
+	}
+}
