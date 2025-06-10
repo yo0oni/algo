a3cde829dbca64cf0acbe418cb7871b67134336f
Jiyoon Kim <77226122+yo0oni@users.noreply.github.com>
2025-06-10 20:35:29 +0900

    [Gold III] Title: 소문난 칠공주, Time: 384 ms, Memory: 158952 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -3,104 +3,73 @@ import java.util.*;
 
 public class Main {
 
-	static int[] di = { 0, 1, 0, -1 };
-	static int[] dj = { 1, 0, -1, 0 };
-	static ArrayList<int[]> positions;
-	static char[][] board;
-	static int totalCount = 0;
+    static char[][] board = new char[5][5];
+    static boolean[] selected = new boolean[25];
+    static int totalCount = 0;
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		// 소문난 칠공주 조건
-		// 7명
-		// 가로나 세로 인접
-		// 반드시 이다솜파가 아니어도 됨
-		// but 이다솜파가 4명 이상이어야함
-		board = new char[5][5];
-		positions = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            board[i] = br.readLine().toCharArray();
+        }
 
-		for (int i = 0; i < 5; i++) {
-			char[] line = br.readLine().toCharArray();
+        dfs(0, 0, 0);
+        System.out.println(totalCount);
+    }
 
-			for (int j = 0; j < 5; j++) {
-				positions.add(new int[] { i, j });
-				board[i][j] = line[j];
-			}
-		}
+    // 조합을 생성하면서 동시에 S 개수 세기
+    static void dfs(int idx, int depth, int sCount) {
+        if (depth - sCount > 3) return; // Y가 4명 이상이면 가지치기
+        if (depth == 7) {
+            if (isConnected()) totalCount++;
+            return;
+        }
 
-		dfs(0, 0, new ArrayList<>());
-		System.out.println(totalCount);
+        for (int i = idx; i < 25; i++) {
+            selected[i] = true;
+            int r = i / 5;
+            int c = i % 5;
+            dfs(i + 1, depth + 1, sCount + (board[r][c] == 'S' ? 1 : 0));
+            selected[i] = false;
+        }
+    }
 
-		System.out.println();
+    // 선택된 7명이 연결되어 있는지 BFS로 확인
+    static boolean isConnected() {
+        boolean[][] visited = new boolean[5][5];
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+        int count = 0;
 
-	}
+        for (int i = 0; i < 25; i++) {
+            if (selected[i]) {
+                int r = i / 5;
+                int c = i % 5;
+                queue.offer(new int[]{r, c});
+                visited[r][c] = true;
+                break;
+            }
+        }
 
-	private static void dfs(int depth, int start, ArrayList<int[]> girls) {
-		if (girls.size() > 7) {
-			return;
-		}
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            count++;
 
-		if (girls.size() == 7) {
-			if (connect(girls)) {
-				totalCount++;
-				return;
-			}
-		}
+            for (int d = 0; d < 4; d++) {
+                int nr = cur[0] + dx[d];
+                int nc = cur[1] + dy[d];
+                int nextIdx = nr * 5 + nc;
 
-		for (int i = start; i < 25; i++) {
-			girls.add(positions.get(i));
-			dfs(depth + 1, i + 1, girls);
-			girls.remove(girls.size() - 1);
-		}
-	}
+                if (nr < 0 || nr >= 5 || nc < 0 || nc >= 5) continue;
+                if (!selected[nextIdx] || visited[nr][nc]) continue;
 
-	// bfs
-	private static boolean connect(ArrayList<int[]> girls) {
-		int sCount = 0;
-		int yCount = 0;
-		boolean isConnected = false;
-		boolean[][] visited = new boolean[5][5];
+                visited[nr][nc] = true;
+                queue.offer(new int[]{nr, nc});
+            }
+        }
 
-		int[] start = girls.get(0);
-		visited[start[0]][start[1]] = true;
-		ArrayDeque<int[]> dq = new ArrayDeque<>();
-		dq.add(new int[] { start[0], start[1] });
-
-		if (board[start[0]][start[1]] == 'S') {
-			sCount++;
-		} else {
-			yCount++;
-		}
-
-		while (!dq.isEmpty()) {
-			int[] current = dq.poll();
-			int ci = current[0];
-			int cj = current[1];
-
-			for (int d = 0; d < 4; d++) {
-				int ni = ci + di[d];
-				int nj = cj + dj[d];
-
-				for (int[] next : girls) {
-					if (ni == next[0] && nj == next[1] && !visited[ni][nj]) {
-						dq.add(new int[] { ni, nj });
-						visited[ni][nj] = true;
-
-						if (board[ni][nj] == 'S') {
-							sCount++;
-						} else {
-							yCount++;
-						}
-					}
-				}
-			}
-		}
-
-		if (sCount >= 4 && sCount + yCount == 7) {
-			isConnected = true;
-		}
-
-		return isConnected;
-	}
+        return count == 7;
+    }
 }
