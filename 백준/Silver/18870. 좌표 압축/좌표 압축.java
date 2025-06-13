f81593db224d7faac03e4caedace84f8c446e83a
Jiyoon Kim <77226122+yo0oni@users.noreply.github.com>
2025-06-13 22:17:17 +0900

    [Silver II] Title: 좌표 압축, Time: 1820 ms, Memory: 269228 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -2,30 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException{
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
-		
-		int arr[]=new int[N];
-		int sort[]=new int[N];
-		HashMap<Integer,Integer> map = new HashMap<>();
-		StringTokenizer st= new StringTokenizer(br.readLine()," ");
-		
-		for(int i=0; i<N; i++) 
-			arr[i]=Integer.parseInt(st.nextToken());
+
+		int arr[] = new int[N];
+		int sort[] = new int[N];
+		HashMap<Integer, Integer> map = new HashMap<>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
 		sort = arr.clone();
 		Arrays.sort(sort);
-		int index=0;
-		for(int i=0; i<N; i++) {
-			if(!map.containsKey(sort[i]))
+
+		int index = 0;
+		for (int i = 0; i < N; i++) {
+			if (!map.containsKey(sort[i]))
 				map.put(sort[i], index++);
 		}
-		for(int i=0; i<N; i++)
-			sb.append(map.get(arr[i])+" ");
+
+		for (int i = 0; i < N; i++)
+			sb.append(map.get(arr[i])).append(" ");
+
 		System.out.print(sb);
-		br.close();
-	    
+
 	}
 }
