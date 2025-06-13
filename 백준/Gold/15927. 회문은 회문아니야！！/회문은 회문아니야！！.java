8d8318a8c04b49ee2e6411f2c730e4ce7b4c2a7f
Jiyoon Kim <77226122+yo0oni@users.noreply.github.com>
2025-06-13 16:12:42 +0900

    [Gold V] Title: 회문은 회문아니야!!, Time: 208 ms, Memory: 18748 KB -BaekjoonHub

---
+++ 백준/Gold/15927. 회문은 회문아니야！！/회문은 회문아니야！！.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        if (!isPalindrome(s)) {
+            System.out.println(s.length());
+        } else if (allCharsSame(s)) {
+            System.out.println(-1);
+        } else {
+            System.out.println(s.length() - 1);
+        }
+    }
+
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static boolean allCharsSame(String s) {
+        char first = s.charAt(0);
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) != first) return false;
+        }
+        return true;
+    }
+}
