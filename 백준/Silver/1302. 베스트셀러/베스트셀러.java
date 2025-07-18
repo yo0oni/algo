07f24931ff67b6935ad66af39aec44b248308353
Jiyoon Kim <77226122+yo0oni@users.noreply.github.com>
2025-07-18 09:21:37 +0900

    [Silver IV] Title: 베스트셀러, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.java
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -5,6 +5,24 @@ public class Main {
     static HashMap<String, Integer> count;
     static int N;
 
+    static class Word implements Comparable<Word> {
+        String word;
+        int count;
+
+        public Word(String word, int count) {
+            this.word = word;
+            this.count = count;
+        }
+
+        @Override
+        public int compareTo(Word o) {
+            if(o.count != this.count) {
+                return o.count - this.count;
+            }
+            return this.word.compareTo(o.word);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         count = new HashMap<>();
@@ -13,16 +31,16 @@ public class Main {
         for(int i = 0; i < N; i++){
             String title = br.readLine();
             if(!count.containsKey(title)){
-                count.put(title, -1);
+                count.put(title, 1);
             } else {
-                count.put(title, count.get(title) - 1);
+                count.put(title, count.get(title) + 1);
             }
         }
 
-        ArrayList<String> keys =  new ArrayList<>(count.keySet());
-        keys.sort(Comparator.comparing((String a) -> count.get(a))
-                .thenComparing(a -> a));
-
-        System.out.println(keys.get(0));
+        PriorityQueue<Word> pq = new PriorityQueue<>();
+        for(String word : count.keySet()){
+            pq.add(new Word(word, count.get(word)));
+        }
+        System.out.println(pq.peek().word);
     }
 }
